879345f179836c16c6b79cdbdaa63d90f6792e17
Keith Wade <keawade@gmail.com>
2017-01-27 08:11:29 -0600

    Switch to using promises

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/room', function (req, res) {
       res.send(rooms)
     })
   } catch (err) {
-    res.status(400).send(err)
+    res.status(500).send(err)
   }
 })
 
@@ -40,31 +40,28 @@ app.post('/room', function (req, res) {
       name: req.body.name,
       reservations: []
     })
-    room.save(function (err, newRoom) {
-      if (err) {
-        console.error('failed to create room', err)
-        res.status(500).send(err)
-        return
-      }
-      res.send({
-        _id: newRoom._id,
-        name: newRoom.name,
-        reservations: newRoom.reservations
+    room.save()
+      .then((newRoom) => {
+        console.log('created new room', newRoom._id)
+        res.send({
+          _id: newRoom._id,
+          name: newRoom.name,
+          reservations: newRoom.reservations
+        })
       })
-    })
+      .catch((error) => {
+        console.error('failed to create room', error)
+        res.status(500).send(error)
+      })
   } catch (err) {
-    res.status(400).send(err)
+    res.status(500).send(err)
   }
 })
 
 app.get('/room/:id', function (req, res) {
   try {
-    Room.findOne({ '_id': req.params.id }, '_id name reservations', function (err, room) {
-      if (err) {
-        console.error('error:', err)
-        res.status(500).send(err)
-        return
-      }
+    Room.findOne({ '_id': req.params.id }, '_id name reservations')
+    .then((room) => {
       if (room) {
         console.log('found room', room)
         res.send(room)
@@ -73,72 +70,82 @@ app.get('/room/:id', function (req, res) {
         res.send('Error: room not found')
       }
     })
+    .catch((error) => {
+      console.error('error:', error)
+      res.status(500).send(error)
+    })
   } catch (err) {
-    res.status(400).send(err)
+    res.status(500).send(err)
   }
 })
 
 app.put('/room/:id', function (req, res) {
   try {
-    Room.findOne({ '_id': req.params.id }, function (err, room) {
-      if (err) {
-        console.error('error:', err)
-        res.status(500).send(err)
-        return
-      }
-      room.name = req.body.name ? req.body.name : room.name
-      room.reservations = req.body.reservations ? req.body.reservations : room.reservations
-      room.save(function (err, updatedRoom) {
-        if (err) {
-          console.error('error:', err)
-          res.status(500).send(err)
-          return
-        }
-        res.send({
-          _id: updatedRoom._id,
-          name: updatedRoom.name,
-          reservations: updatedRoom.reservations
-        })
+    Room.findOne({ '_id': req.params.id })
+      .then((room) => {
+        room.name = req.body.name ? req.body.name : room.name
+        room.reservations = req.body.reservations ? req.body.reservations : room.reservations
+        room.save()
+          .then((updatedRoom) => {
+            console.log('updated room', updatedRoom._id)
+            res.send({
+              _id: updatedRoom._id,
+              name: updatedRoom.name,
+              reservations: updatedRoom.reservations
+            })
+          })
+          .catch((error) => {
+            console.error('error:', error)
+            res.status(500).send(error)
+          })
       })
-    })
+      .catch((error) => {
+        console.error('error:', error)
+        res.status(500).send(error)
+      })
   } catch (err) {
-    res.status(400).send(err)
+    res.status(500).send(err)
   }
 })
 
 app.delete('/room/:id', function (req, res) {
   try {
-    Room.findOne({ '_id': req.params.id }, function (err, room) {
-      if (err) {
-        console.error('error:', err)
-        res.status(500).send(err)
-        return
-      }
-      room.reservations.map((roomReserv) => {
-        Reservation.findOne({ '_id': roomReserv._id }, function (err, reservation) {
-          if (err) {
-            console.error('error:', err)
-            return
-          }
-          reservation.remove(function (err) {
-            if (err) {
-              console.error('error:', err)
-              return
-            }
+    Room.findOne({ '_id': req.params.id })
+      .then((room) => {
+        const promises = room.reservations.map((roomReserv) => {
+          return Reservation.findOne({ '_id': roomReserv._id })
+        })
+        Promise.all(promises)
+          .then((reservations) => {
+            const delPromises = reservations.map((reservation) => {
+              return reservation.remove()
+            })
+            Promise.all(delPromises)
+              .then(() => {
+                console.log('deleted reservations')
+                room.remove()
+                  .then(() => {
+                    res.send('deleted')
+                  })
+                  .catch((error) => {
+                    console.error('error:', error)
+                    res.status(500).send(error)
+                  })
+              })
+              .catch((error) => {
+                console.error('failed to delete reservation', error)
+              })
           })
+          .catch((error) => {
+            console.log('failed to find reservations', error)
+          })
+        .catch((error) => {
+          console.error('error:', error)
+          res.status(500).send(error)
         })
       })
-      room.remove(function (err) {
-        if (err) {
-          console.error('error:', err)
-          res.status(500).send(err)
-          return
-        }
-        res.send('deleted')
-      })
-    })
   } catch (err) {
-    res.status(400).send(err)
+    res.status(500).send(err)
   }
 })
 
@@ -162,47 +169,47 @@ app.post('/reservation', function (req, res) {
       start: req.body.start,
       end: req.body.end
     })
-    reservation.save(function (err, newReservation) {
-      if (err) {
-        console.error('failed to create reservation', err)
-        res.status(500).send(err)
-        return
-      }
-      console.log('created reservation', newReservation._id)
-      res.send({
-        _id: newReservation._id,
-        meetingName: newReservation.meetingName,
-        owner: newReservation.owner,
-        ownerEmail: newReservation.ownerEmail,
-        start: newReservation.start,
-        end: newReservation.end
+    reservation.save()
+      .then((newReservation) => {
+        console.log('created reservation', newReservation._id)
+        res.send({
+          _id: newReservation._id,
+          meetingName: newReservation.meetingName,
+          owner: newReservation.owner,
+          ownerEmail: newReservation.ownerEmail,
+          start: newReservation.start,
+          end: newReservation.end
+        })
       })
-    })
+      .catch((error) => {
+        console.error('failed to create reservation', error)
+        res.status(500).send(error)
+      })
   } catch (err) {
-    res.status(400).send(err)
+    res.status(500).send(err)
   }
 })
 
 app.get('/reservation/:id', function (req, res) {
   try {
-    Reservation.findOne({ '_id': req.params.id }, function (err, reservation) {
-      if (err) {
-        console.error('error:', err)
-        res.status(500).send(err)
-        return
-      }
-      console.log('got reservation', reservation._id)
-      res.send({
-        _id: reservation._id,
-        meetingName: reservation.meetingName,
-        owner: reservation.owner,
-        ownerEmail: reservation.ownerEmail,
-        start: reservation.start,
-        end: reservation.end
+    Reservation.findOne({ '_id': req.params.id })
+      .then((reservation) => {
+        console.log('got reservation', reservation._id)
+        res.send({
+          _id: reservation._id,
+          meetingName: reservation.meetingName,
+          owner: reservation.owner,
+          ownerEmail: reservation.ownerEmail,
+          start: reservation.start,
+          end: reservation.end
+        })
       })
-    })
+      .catch((error) => {
+        console.error('error:', error)
+        res.status(500).send(error)
+      })
   } catch (err) {
-    res.status(400).send(err)
+    res.status(500).send(err)
   }
 })
 
@@ -218,34 +225,34 @@ app.put('/reservation/:id', function (req, res) {
       console.warn('failed to update reservation: missing body arguments')
       res.status(400).send('Invalid body')
     }
-    Reservation.findOne({ '_id': req.params.id }, function (err, reservation) {
-      if (err) {
-        console.warn('could not find reservation', req.params.id, err)
-        res.status(404).send(err)
-        return
-      }
-      reservation.meetingName = req.body.meetingName ? req.body.meetingName : reservation.meetingName
-      reservation.owner = req.body.owner ? req.body.owner : reservation.owner
-      reservation.ownerEmail = req.body.ownerEmail ? req.body.ownerEmail : reservation.ownerEmail
-      reservation.start = req.body.start ? req.body.start : reservation.start
-      reservation.end = req.body.end ? req.body.end : reservation.end
-      reservation.save(function (err, updatedReservation) {
-        if (err) {
-          console.error('failed to update reservation', err)
-          res.status(500).send(err)
-          return
-        }
-        console.log('updated reservation', updatedReservation._id)
-        res.send({
-          _id: updatedReservation._id,
-          meetingName: updatedReservation.meetingName,
-          owner: updatedReservation.owner,
-          ownerEmail: updatedReservation.ownerEmail,
-          start: updatedReservation.start,
-          end: updatedReservation.end
-        })
+    Reservation.findOne({ '_id': req.params.id })
+      .then((reservation) => {
+        reservation.meetingName = req.body.meetingName ? req.body.meetingName : reservation.meetingName
+        reservation.owner = req.body.owner ? req.body.owner : reservation.owner
+        reservation.ownerEmail = req.body.ownerEmail ? req.body.ownerEmail : reservation.ownerEmail
+        reservation.start = req.body.start ? req.body.start : reservation.start
+        reservation.end = req.body.end ? req.body.end : reservation.end
+        reservation.save()
+          .then((updatedReservation) => {
+            console.log('updated reservation', updatedReservation._id)
+            res.send({
+              _id: updatedReservation._id,
+              meetingName: updatedReservation.meetingName,
+              owner: updatedReservation.owner,
+              ownerEmail: updatedReservation.ownerEmail,
+              start: updatedReservation.start,
+              end: updatedReservation.end
+            })
+          })
+          .catch((error) => {
+            console.error('failed to update reservation', error)
+            res.status(500).send(error)
+          })
       })
-    })
+      .catch((error) => {
+        console.warn('could not find reservation', req.params.id, error)
+        res.status(404).send(error)
+      })
   } catch (err) {
     res.status(500).send(err)
   }
@@ -253,23 +260,23 @@ app.put('/reservation/:id', function (req, res) {
 
 app.delete('/reservation/:id', function (req, res) {
   try {
-    Reservation.findOne({ '_id': req.params.id }, function (err, reservation) {
-      if (err) {
-        console.error('failed to find reservation for deletion', req.params.id, err)
-        res.status(404).send(err)
-        return
-      }
-      reservation.remove(function (err) {
-        if (err) {
-          console.error('failed to delete reservation', req.params.id, err)
-          res.status(500).send(err)
-          return
-        }
-        res.send('deleted')
+    Reservation.findOne({ '_id': req.params.id })
+      .then((reservation) => {
+        reservation.remove()
+          .then(() => {
+            res.send('deleted')
+          })
+          .catch((error) => {
+            console.error('failed to delete reservation', req.params.id, error)
+            res.status(500).send(error)
+          })
       })
-    })
+      .catch((error) => {
+        console.error('failed to find reservation for deletion', req.params.id, error)
+        res.status(404).send(error)
+      })
   } catch (err) {
-    res.status(400).send(err)
+    res.status(500).send(err)
   }
 })
 
