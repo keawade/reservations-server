190f223dfa5a6c52eef4adf04e5dad10ca1955db
Nathan Bland <nathan.bland@gmail.com>
2017-10-26 14:06:13 -0600

    Adding CORS fix for cross-domain requests. Fixes #5

--- server.js
+++ server.js
@@ -12,6 +12,20 @@ const app = express()
 
 app.use(bodyParser.json())
 
+app.use(function (req, res, next) {
+    /*
+    * First header allows any domain to make a request to the api.
+    * Second header allows credentials to be passed as well incase authentication is ever desired.
+    * Third line allows important client request headers to be set.
+    * Last line dictates which HTTP methods are allowed. For now, that's all of them.
+    */
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS, PATCH')
+    next()
+})  
+
 app.use('/', routes)
 
 console.log('listening at localhost:3000')
